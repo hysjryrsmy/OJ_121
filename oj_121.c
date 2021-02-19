1b37e9f8f1688045142a363a7c50afc319870fda
yiyi <1484491854a@gmail.com>
2021-02-19 14:24:55 +0900

    石头剪刀布

---
+++ oj_121.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	char a, b;
+	scanf("%c %c", &a, &b);
+	if ((a == 'O' && b == 'Y') || (a == 'Y' && b == 'H') || (a == 'H' && b == 'O'))
+	{
+		printf("MING\n");
+	}
+	if ((a == 'O' && b == 'H') || (a == 'Y' && b == 'O') || (a == 'H' && b == 'Y'))
+	{
+		printf("LI\n");
+	}
+	if ((a == 'O' && b == 'O') || (a == 'Y' && b == 'Y') || (a == 'H' && b == 'H'))
+	{
+		printf("TIE\n");
+	}
+	return 0;
+}
+
+
